e07f28f1c3303558b9e409e76978fea2f331ab43
Bernard <bvdv9075@gmail.com>
2021-10-19 11:27:11 +0100

    added nodered json

--- pumpkin.ino
+++ pumpkin.ino
@@ -1,18 +1,16 @@
-#include <Arduino.h>
-#include <Adafruit_NeoPixel.h>
 #include <WiFi.h>
 #include <WebServer.h>
 #include <ArduinoJson.h>
-#include <FreeRTOS.h>
+#include <Adafruit_NeoPixel.h>
 #include <HTTPClient.h>
 
-const char *SSID = "Midian";
-const char *PWD = "0829688988#";
-#define MOTION_PIN 2
+// Variables
 #define RING_PIN 15
 #define NUMPIXELS 12
-String response = "";
-Adafruit_NeoPixel pixels = Adafruit_NeoPixel(NUMPIXELS, RING_PIN, NEO_GRB + NEO_KHZ800);
+#define MOTION_PIN 6
+
+String response;
+
 // Web server running on port 80
 WebServer server(80);
 
@@ -20,6 +18,11 @@ WebServer server(80);
 StaticJsonDocument<250> jsonDocument;
 char buffer[250];
 
+//LED Ring Init
+Adafruit_NeoPixel pixels = Adafruit_NeoPixel(NUMPIXELS, RING_PIN, NEO_GRB + NEO_KHZ800);
+
+const char *SSID = "Midian";
+const char *PWD = "0829688988#";
 
 void connectToWiFi() {
   Serial.print("Connecting to ");
@@ -28,8 +31,8 @@ void connectToWiFi() {
   WiFi.begin(SSID, PWD);
 
   while (WiFi.status() != WL_CONNECTED) {
-    Serial.print(".");
     delay(500);
+    Serial.print(".");
     // we can even make the ESP32 to sleep
   }
 
@@ -38,10 +41,17 @@ void connectToWiFi() {
 }
 
 void setup_routing() {
-//  server.on("/motion", motionOn);
-//  server.on("/ringoff", ringOff); 
-//  // start server
-//  server.begin();
+  server.on("/motion", motionOn);
+  server.on("/ringoff", ringOff); 
+  // start server
+  server.begin();
+}
+
+void add_json_object(char *tag, char *value, char *unit) {
+  JsonObject obj = jsonDocument.createNestedObject();
+  obj["type"] = tag;
+  obj["value"] = value;
+  obj["unit"] = unit;
 }
 
 void create_json(char *tag, int value, char *unit) {
@@ -50,13 +60,6 @@ void create_json(char *tag, int value, char *unit) {
   jsonDocument["value"] = value;
   jsonDocument["unit"] = unit;
   serializeJson(jsonDocument, buffer);
-}
-
-void add_json_object(char *tag, char *value, char *unit) {
-  JsonObject obj = jsonDocument.createNestedObject();
-  obj["type"] = tag;
-  obj["value"] = value;
-  obj["unit"] = unit;
 }
 
 void motionOn() {
@@ -86,37 +89,33 @@ void ringOff() {
 }
 
 void setup() {
+  Serial.begin(115200);
   connectToWiFi();
-//  setup_routing();
-//  pixels.begin();
-//  pinMode(RING_PIN, INPUT);
-//  Serial.begin(115200);
-  
-//  pixels.setBrightness(25);  // Lower brightness and save eyeballs!
-//  pixels.show(); // Initialize all pixels to 'off'
+  setup_routing();
+  pixels.begin();
+  pinMode(RING_PIN, INPUT);
 }
 
 void loop() 
 {
-  
   server.handleClient();
-//
-//  HTTPClient http;
-//  //The API URL
-//  String serverName = "http://bernard:Taniam072@192.168.1.106:1880/endpoint/test";
-//  bool isDetected = digitalRead(MOTION_PIN);
-//  const char* motion;
-//  motion = "off";
-//  if (isDetected) {
-//    motion = "on";
-//  }
-////
-////  //Start the request
-//  http.begin(serverName + "?temperature=" + motion);
-//  //Use HTTP GET request
-//  delay(1000);
-//  http.GET();
-//  //Response from server
-//  response = http.getString();
+  HTTPClient http;
+  //The API URL
+  String serverName = "http://bernard:Taniam072@192.168.1.106:1880/endpoint/test";
+  bool isDetected = digitalRead(MOTION_PIN);
+  delay(50);
+  const char* motion;
+  motion = "off";
+  if (isDetected) {
+    Serial.print("Motion Detected");
+    motion = "on";
+  }
 
+  //Start the request
+  http.begin(serverName + "?temperature=" + motion);
+  //Use HTTP GET request
+  delay(5000);
+  http.GET();
+  //Response from server
+  response = http.getString();
 }
